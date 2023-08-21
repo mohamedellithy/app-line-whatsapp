73b4d760f26bb36da8f65b481efbdc6e44dfcf37
mohamedellithy <develomohamed22@gmail.com>
2023-08-21 10:05:12 +0300

    dkwdwس

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -13,7 +13,6 @@ class AppController extends Controller
         Http::post('https://webhook.site/f032ba41-f451-4aba-a8b3-a97fbff114de',$request->all());
     }
     public function make_event(Request $request){
-        dd('hi');
         Http::get('https://webhook.site/f032ba41-f451-4aba-a8b3-a97fbff114de');
     }
 }
