020074d8c4d233928bfd15fee8e466d6c0444208
mohamedellithy <develomohamed22@gmail.com>
2023-08-21 13:05:55 +0300

    dkwdwswddwdsdwesdfw

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -14,7 +14,7 @@ class AppController extends Controller
 
     public function make_event(Request $request){
        $event = new AppEvents();
-       $result = $event->get_json_data();
-       Http::get('https://webhook.site/f032ba41-f451-4aba-a8b3-a97fbff114de',$result);
+       $result = $event->make_event();
+       Http::get('https://webhook.site/f032ba41-f451-4aba-a8b3-a97fbff114de');
     }
 }
