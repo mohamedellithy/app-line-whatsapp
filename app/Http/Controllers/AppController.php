1015bfe25f8071c84005513a6a02608163a0f76d
mohamedellithy <develomohamed22@gmail.com>
2023-08-21 11:44:53 +0300

    dkwdwswd

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -8,12 +8,11 @@ use Illuminate\Support\Facades\Http;
 class AppController extends Controller
 {
     //
-
     public function salla_callback(Request $request){
         Http::post('https://webhook.site/f032ba41-f451-4aba-a8b3-a97fbff114de',$request->all());
     }
     public function make_event(Request $request){
        $send =  Http::get('https://webhook.site/f032ba41-f451-4aba-a8b3-a97fbff114de');
-       dd($send->json());
+       dd($send->body());
     }
 }
