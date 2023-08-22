df0b8144febbc381fdbeceb382ea0efd97f36a83
mohamedellithy <develomohamed22@gmail.com>
2023-08-22 10:13:48 +0300

    dkwdwwdhdddسdsssdsdwb

--- app/Services/SallaServices/User.php
+++ app/Services/SallaServices/User.php
@@ -104,16 +104,15 @@ class User{
                     $access_token = '649ba622aa900';
                     $temp = '201026051966' ?: $merchant_credentails->phone;
                     // message text
-                    // $message = urlencode("
-                    //     تهانيا 
-                    //     تم انشاء حسابك على منصة line.sa بنجاح 
-                    //     تفاصيل الحساب 
-                    //     البريد الالكترونى : {$new_account->email} \n
-                    //     اسم المستخدم : {$new_account->fullname} \n
-                    //     كلمة المرور  : {$password} \n
-                    //     رابط المنصة : {$platform_link} \n
-                    // ");
-                    $message = 'hi';
+                    $message = urlencode("
+                        تهانيا
+                        تم انشاء حسابك على منصة line.sa بنجاح
+                        تفاصيل الحساب
+                        البريد الالكترونى : {$new_account->email} \n
+                        اسم المستخدم : {$new_account->fullname} \n
+                        كلمة المرور  : {$password} \n
+                        رابط المنصة : {$platform_link} \n
+                    ");
 
                     // send message with all info and it was installed succefully
                     $karzoun_send_message   = Http::post(
