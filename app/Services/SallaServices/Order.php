8b31ccf2026169f9eb9dbc2ac4c9021a5e31c355
mohamedellithy <develomohamed22@gmail.com>
2023-08-26 17:47:35 +0300

    djddded

--- app/Services/SallaServices/Order.php
+++ app/Services/SallaServices/Order.php
@@ -27,7 +27,10 @@ class Order extends AppMerchant implements AppEvent{
         $log = json_encode($this->data, JSON_UNESCAPED_UNICODE) . PHP_EOL;
 
         // set log data
-        Log::channel('settings_update_events')->info($log);
+        Log::build([
+            'driver' => 'single',
+            'path' => storage_path('logs/salla_events.log'),
+        ])->info($log);
     }
 
     public function resolve_event(){
