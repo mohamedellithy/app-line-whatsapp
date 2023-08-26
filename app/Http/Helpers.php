78b404ce2b8368da6ecd37c84f03516bc6989072
mohamedellithy <develomohamed22@gmail.com>
2023-08-26 17:23:09 +0300

    fiddc

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -8,6 +8,10 @@ function formate_order_details($order_details){
     $attrs['payment_method'] = $order_details['data']['payment_method'];
     $attrs['currency']       = $order_details['data']['currency'];
     $attrs['amounts']        = $order_details['data']['amounts']['total']['amount'];
+    $attrs['customer']       = $order_details['data']['customer'];
+    $attrs['items']          = $order_details['data']['items'];
+    $attrs['bank']           = $order_details['data']['bank'];
+
 
     return $attrs;
 
