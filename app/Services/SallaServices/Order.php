8c4c1317267a4a15fc6694d3dbf3bfe7cc2d48cd
mohamedellithy <develomohamed22@gmail.com>
2023-08-26 19:55:06 +0300

    djdddedewddddddeee

--- app/Services/SallaServices/Order.php
+++ app/Services/SallaServices/Order.php
@@ -69,7 +69,7 @@ class Order extends AppMerchant implements AppEvent{
                 'status' => $result_send_message
             ]);
 
-            $app_event->increament('count_of_call');
+            $app_event->increment('count_of_call');
 
             Http::post('https://webhook.site/19694e58-fa42-41d5-a247-2187b0718cf7',$this->data);
         endif;
