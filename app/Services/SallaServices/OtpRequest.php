9dcac9f8844d5162a10f94421e8288f8c03daca7
mohamedellithy <develomohamed22@gmail.com>
2023-11-04 11:13:45 +0200

    dkdkصddd

--- app/Services/SallaServices/OtpRequest.php
+++ app/Services/SallaServices/OtpRequest.php
@@ -67,6 +67,7 @@ class OtpRequest extends AppMerchant implements AppEvent{
         if(filter_var($this->data['data']['contact'],FILTER_VALIDATE_EMAIL)) return;
 
         if($app_event->status != 'success'):
+            Log::info($this->merchant_team->account);
             $message = isset($this->settings['otp_message']) ? $this->settings['otp_message'] : '';
             $filter_message = message_order_params($message, $attrs);
             $result_send_message = send_message(
