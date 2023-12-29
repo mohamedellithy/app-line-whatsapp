d1a04bb10cb21465d21c2c7a7195ad1b5e436349
mohamedellithy <develomohamed22@gmail.com>
2023-12-29 13:55:59 +0200

    dssc

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -150,7 +150,9 @@ if(!function_exists('send_message_error')):
             $end_point    = "https://wh.line.sa/api/send?number=$phone_number&type=text&message=$message&instance_id=$instance_id&access_token=$access_token";
         endif;
         $send_result         = Http::post($end_point);
-        return $send_result->json();
+        return [
+            'result ' => $send_result->json()
+        ];
     }
 endif;
 
