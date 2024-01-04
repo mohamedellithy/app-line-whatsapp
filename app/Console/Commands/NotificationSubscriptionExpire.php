d415ef0bef6fca5d468390fae480f73bf93b53fa
mohamedellithy <develomohamed22@gmail.com>
2024-01-04 14:38:01 +0200

    ddddsds

--- app/Console/Commands/NotificationSubscriptionExpire.php
+++ app/Console/Commands/NotificationSubscriptionExpire.php
@@ -33,7 +33,13 @@ class NotificationUsersPrivate extends Command
     public function handle()
     {
         $today = Carbon::today();
-        $users = SpUser::with('merchant_info')->where('expiration_date','>',$today->timestamp)->first();
+        $users = SpUser::with('merchant_info','team','team.account')->where('expiration_date','>',$today->timestamp)->first();
+        
+        // ->chunk(50,function($rows){
+        //     foreach($rows as $row):
+        //         $phone = 
+        //     endforeach;
+        // });
         //
         Http::withOptions([
             'verify' => false
