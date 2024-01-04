8415397f848b039837fe8e71cca8e62261f75528
mohamedellithy <develomohamed22@gmail.com>
2024-01-04 17:27:11 +0200

    dwsddddي

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -41,12 +41,12 @@ class Kernel extends ConsoleKernel
 
         ///////////////////////////////////////////////////////////////////////
         $random_minutes = [
-            'everyMinute',
-            // 'everyFiveMinutes',
-            // 'everyTenMinutes',
-            // 'everyFifteenMinutes',
-            // 'everyThirtyMinutes',
-            // 'hourly'
+            //'everyMinute',
+            'everyFiveMinutes',
+            'everyTenMinutes',
+            'everyFifteenMinutes',
+            'everyThirtyMinutes',
+            'hourly'
         ];
 
         $key_nump = array_rand($random_minutes,1);
