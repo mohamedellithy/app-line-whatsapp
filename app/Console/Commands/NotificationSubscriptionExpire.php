d0587d086479aa952b38233c9aae524dcc7fac43
mohamedellithy <develomohamed22@gmail.com>
2024-01-06 13:55:22 +0200

    dddddd

--- app/Console/Commands/NotificationSubscriptionExpire.php
+++ app/Console/Commands/NotificationSubscriptionExpire.php
@@ -42,40 +42,42 @@ class NotificationUsersPrivate extends Command
         ->where('expiration_date','!=',0)
         ->where('expiration_date','<=',$today->timestamp)->first();
 
-        if($row->team->account->pid):
-            $formate_phone = explode('@',$row->team->account->pid);
-            $phone = $formate_phone[0] ?: '';
-            if(count($row->merchant_info) == 0):
-                $message = "
-                مرحبًا ، ".$row->username."
-                نأمل أن تكون بخير وتستمتع بخدمتنا. نود أن نذكرك بأن اشتراكك الحالي انتهى. ونحن نود أن نقدم لك فرصة لتجديد اشتراكك والاستمرار في الاستفادة من جميع المزايا والميزات التي نقدمها.
-                لتجديد اشتراكك، يُرجى زيارة الرابط التالي مباشرة:
-                https://line.sa/19505
-                نشكرك مرة أخرى على ثقتك فينا ونتطلع إلى مواصلة خدمتك. نحن ممتنون لك كعميل ونعدك بأننا سنبذل قصارى جهدنا لتلبية توقعاتك.
-                مع خالص الود,
-                خدمة عملاء واتساب لاين
-                ";
-            else:
-                $message = "
-                مرحبًا ، ".$row->username."
-                نأمل أن تكون بخير وتستمتع بخدمتنا. نود أن نذكرك بأن اشتراكك الحالي انتهى. ونحن نود أن نقدم لك فرصة لتجديد اشتراكك والاستمرار في الاستفادة من جميع المزايا والميزات التي نقدمها.
-                لتجديد اشتراكك او ترقية الاشتراك ، يُرجى زيارة الرابط التالي مباشرة:
-                https://s.salla.sa/apps/install/1662840947?upgrade=1
-                نشكرك مرة أخرى على ثقتك فينا ونتطلع إلى مواصلة خدمتك. نحن ممتنون لك كعميل ونعدك بأننا سنبذل قصارى جهدنا لتلبية توقعاتك.
-                مع خالص الود,
-                خدمة عملاء واتساب لاين
-                ";
+        if($row->team):
+            if($row->team->account->pid):
+                $formate_phone = explode('@',$row->team->account->pid);
+                $phone = $formate_phone[0] ?: '';
+                if(count($row->merchant_info) == 0):
+                    $message = "
+                    مرحبًا ، ".$row->username."
+                    نأمل أن تكون بخير وتستمتع بخدمتنا. نود أن نذكرك بأن اشتراكك الحالي انتهى. ونحن نود أن نقدم لك فرصة لتجديد اشتراكك والاستمرار في الاستفادة من جميع المزايا والميزات التي نقدمها.
+                    لتجديد اشتراكك، يُرجى زيارة الرابط التالي مباشرة:
+                    https://line.sa/19505
+                    نشكرك مرة أخرى على ثقتك فينا ونتطلع إلى مواصلة خدمتك. نحن ممتنون لك كعميل ونعدك بأننا سنبذل قصارى جهدنا لتلبية توقعاتك.
+                    مع خالص الود,
+                    خدمة عملاء واتساب لاين
+                    ";
+                else:
+                    $message = "
+                    مرحبًا ، ".$row->username."
+                    نأمل أن تكون بخير وتستمتع بخدمتنا. نود أن نذكرك بأن اشتراكك الحالي انتهى. ونحن نود أن نقدم لك فرصة لتجديد اشتراكك والاستمرار في الاستفادة من جميع المزايا والميزات التي نقدمها.
+                    لتجديد اشتراكك او ترقية الاشتراك ، يُرجى زيارة الرابط التالي مباشرة:
+                    https://s.salla.sa/apps/install/1662840947?upgrade=1
+                    نشكرك مرة أخرى على ثقتك فينا ونتطلع إلى مواصلة خدمتك. نحن ممتنون لك كعميل ونعدك بأننا سنبذل قصارى جهدنا لتلبية توقعاتك.
+                    مع خالص الود,
+                    خدمة عملاء واتساب لاين
+                    ";
+                endif;
+                //$message = trim(preg_replace('/\t/', '', $message));
+
+                // send message with all info and it was installed succefully
+                send_message($phone,$message);
+
+                NotificationSubscriber::create([
+                    'user_id' => $row->id,
+                    'status'  => 'done',
+                    'type'    => 'expiration_date'
+                ]);
             endif;
-            //$message = trim(preg_replace('/\t/', '', $message));
-
-            // send message with all info and it was installed succefully
-            send_message($phone,$message);
-
-            NotificationSubscriber::create([
-                'user_id' => $row->id,
-                'status'  => 'done',
-                'type'    => 'expiration_date'
-            ]);
         endif;
 
         //
