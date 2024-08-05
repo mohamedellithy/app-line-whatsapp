2d52abbf6ec898bd7b6c204e2f10c2b6a48dc02b
mohamedellithy <develomohamed22@gmail.com>
2024-08-05 10:57:29 +0300

    filter excel

--- app/Exports/NumbersExport.php
+++ app/Exports/NumbersExport.php
@@ -17,10 +17,11 @@ class NumbersExport implements FromArray , WithHeadingRow
             $settings = json_decode($merchant->settings,true,5600);
             $phone    = isset($settings['custom_merchant_phone']) ? $settings['custom_merchant_phone'] : $merchant->phone;
             preg_match_all("(\d+)",$phone,$filter_phones);
+            $phones_all = isset($filter_phones[0]) ? ( isset($filter_phones[0][0]) ? $filter_phones[0][0] : null) : null;
             $data[] = [
                 'id'    => $merchant->id,
                 'name'  => $merchant?->user?->fullname ?: $merchant?->user?->username,
-                'phone' => isset($filter_phones[0]) ? ( isset($filter_phones[0][0]) ? $filter_phones[0][0] : null) : null,
+                'phone' => str_replace('"','',$phones_all),
                 'email' => $merchant?->user?->email,
             ];
         endforeach;
