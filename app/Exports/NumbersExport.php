e0e0399a70d6f4d14c1d1e29f53d9b3a21d2e7ad
mohamedellithy <develomohamed22@gmail.com>
2024-08-05 10:47:00 +0300

    filter excel

--- app/Exports/NumbersExport.php
+++ app/Exports/NumbersExport.php
@@ -20,7 +20,7 @@ class NumbersExport implements FromArray , WithHeadingRow
             $data[] = [
                 'id'    => $merchant->id,
                 'name'  => $merchant?->user?->fullname ?: $merchant?->user?->username,
-                'phone' => isset($filter_phones[0]) ? $filter_phones[0][0] : null,
+                'phone' => $filter_phones[0],
                 'email' => $merchant?->user?->email,
             ];
         endforeach;
