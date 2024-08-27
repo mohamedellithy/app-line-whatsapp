6644fdb60e979733760d503d89bca298ff4fdeb2
mohamedellithy <develomohamed22@gmail.com>
2024-08-27 19:06:19 +0300

    first commit

--- app/Http/Controllers/BookingSheetController.php
+++ app/Http/Controllers/BookingSheetController.php
@@ -11,7 +11,7 @@ class BookingSheetController extends Controller
         $client = new \Google\Client();
         $client->setDeveloperKey("AIzaSyAtm5AUR8D0_Zvq5O0eF7WgkMXojeMnYgQ");
         $client->addScope(\Google\Service\Drive::DRIVE);
-        $service = new Google_Service_Sheets($client);
+        $service = new \Google\Service\Sheets\BatchUpdateSpreadsheetRequest($client);
         $result = $service->spreadsheets_values->get("13Jlz0AcBG3DtJcfbFjxmZ9VyXAVw2ekblJRMIi89pIk");
         try {
             $numRows = $result->getValues() != null ? count($result->getValues()) : 0;
