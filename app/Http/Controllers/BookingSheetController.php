6ba45834fab8cfa5bb19db60288bd2c85aa113b6
mohamedellithy <develomohamed22@gmail.com>
2024-08-31 13:32:17 +0300

    fix notification

--- app/Http/Controllers/BookingSheetController.php
+++ app/Http/Controllers/BookingSheetController.php
@@ -12,7 +12,7 @@ class BookingSheetController extends Controller
         $client->setDeveloperKey("AIzaSyAtm5AUR8D0_Zvq5O0eF7WgkMXojeMnYgQ");
         $client->addScope(\Google\Service\Drive::DRIVE);
         $service = new \Google\Service\Sheets($client);
-        $result = $service->spreadsheets_values->get("1xnQe0vsH1fKAliiAWJxPou-7NPu26yMTeMxi7Sq1x3Y","pg1");
+        $result = $service->spreadsheets_values->get("1xnQe0vsH1fKAliiAWJxPou-7NPu26yMTeMxi7Sq1x3Y","pg1!A1:A1");
         try {
             $numRows = $result->getValues() != null ? count($result->getValues()) : 0;
             printf("%d rows retrieved.", $numRows);
