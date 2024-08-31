e0afad32f839241dc2f9ed22954f4425c2609eb3
mohamedellithy <develomohamed22@gmail.com>
2024-08-31 17:20:51 +0300

    fix notification

--- app/Http/Controllers/BookingSheetController.php
+++ app/Http/Controllers/BookingSheetController.php
@@ -11,26 +11,18 @@ class BookingSheetController extends Controller
         $client = new \Google\Client();
         $client->setDeveloperKey("AIzaSyAtm5AUR8D0_Zvq5O0eF7WgkMXojeMnYgQ");
         $client->addScope(\Google\Service\Drive::DRIVE);
-        $service = new \Google\Service\Sheets($client);
-        $result = $service->spreadsheets_values->get("1xnQe0vsH1fKAliiAWJxPou-7NPu26yMTeMxi7Sq1x3Y","pg1!A:A");
-
-        $result2 = $service->spreadsheets_values->get("1xnQe0vsH1fKAliiAWJxPou-7NPu26yMTeMxi7Sq1x3Y","pg1!".count($result->getValues()).":".count($result->getValues()));
+        $service      = new \Google\Service\Sheets($client);
+        $ColumnsA     = $service->spreadsheets_values->get("1xnQe0vsH1fKAliiAWJxPou-7NPu26yMTeMxi7Sq1x3Y","pg1!A:A");
+        $ColumnsCount = count($ColumnsA->getValues());
+        $result       = [];
+        for($i = 1;$i <= $ColumnsCount;$i++){
+            $ColumnItem = $service->spreadsheets_values->get("1xnQe0vsH1fKAliiAWJxPou-7NPu26yMTeMxi7Sq1x3Y","pg1!".$i.":".$i);
+            $result[]   = $ColumnItem->getValues();
+        }
 
         return response()->json([
-            'body'    => $result->getValues(),
-            'result2' => $result2,
-            'counter' => count($result->getValues())
+            'body'    => $result
         ]);
-        // var_dump($result->getValues());
-        // try {
-        //     $numRows = $result->getValues() != null ? count($result->getValues()) : 0;
-        //     printf("%d rows retrieved.", $numRows);
-        //     return $result;
-        // }
-        // catch(\Exception $e) {
-        //     echo 'Message: ' .$e->getMessage();
-        // }
-        // var_dump("hi mohamed");
     }
 
 }
