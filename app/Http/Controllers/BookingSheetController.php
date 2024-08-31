4dfd6f47d8c65661e35ccf7874cd29bc42cd36d0
mohamedellithy <develomohamed22@gmail.com>
2024-08-31 18:27:40 +0300

    fix notification

--- app/Http/Controllers/BookingSheetController.php
+++ app/Http/Controllers/BookingSheetController.php
@@ -43,7 +43,7 @@ class BookingSheetController extends Controller
         if($data['data']['event'] == 'messages.upsert'){
             foreach($data['data']['data']['messages'] as $message):
                 if($message['key']['fromMe'] == false){
-                    $body = $message['ephemeralMessage']['message']['extendedTextMessage']['text'];
+                    $body = $message['message']['ephemeralMessage']['message']['extendedTextMessage']['text'];
                     $client   = new \GuzzleHttp\Client();
                     $send_result         = $client->request(
                         'POST',
