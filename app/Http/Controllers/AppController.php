5b345c889645d5584fc11898cf8a20073116608b
mohamedellithy <develomohamed22@gmail.com>
2024-09-04 18:02:10 +0300

    add admin dashbord

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -16,7 +16,9 @@ class AppController extends Controller
 
         $event_content = file_get_contents('php://input');
         $event         = json_decode($event_content,true);
-       
+        return response()->json([
+            'counter' => $event
+        ]);
         try{
             $event_id = isset($event['data']) ? (isset($event['data']['id']) ? $event['data']['id'] : rand(1,1000)) : rand(1,1000);
             $lock  = Cache::lock("event_no_".$event_id,2);
