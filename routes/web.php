878b046127c2d9ad8dc36225289b2418e4ffa520
mohamedellithy <develomohamed22@gmail.com>
2024-10-03 11:50:14 +0300

    update webhook

--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,8 @@ $router->get('/api/booking-sheet','BookingSheetController@booking_sheet');
 $router->get('/api/sheet-service/{user_id}','BookingSheetController@booking_sheet');
 
 $router->group(['middleware' => 'cors'], function() use ($router) {
-    $router->post('/api/auto-replay','BookingSheetController@auto_replay');
-    $router->get('/api/auto-replay','BookingSheetController@auto_replay');
+    $router->post('/api/auto-replay?id=1','BookingSheetController@auto_replay');
+    $router->get('/api/auto-replay?id=1','BookingSheetController@auto_replay');
 });
 
 // $router->group(['middleware' => 'cors'], function () use ($router) {
