7bb17fa3baad3064c0561cc2b400fdc540dbd592
mohamedellithy <develomohamed22@gmail.com>
2024-10-11 12:04:45 +0300

    update 43

--- app/Services/GoogleSheetServices/GoogleSheetFilterService.php
+++ app/Services/GoogleSheetServices/GoogleSheetFilterService.php
@@ -48,13 +48,13 @@ class GoogleSheetFilterService {
                 'next_question'    => 1,
             ]);
         } elseif(isset($this->google_sheet->current_question)){
-            if($this->booking_sheet_words[$this->google_sheet->next_question] != 'موعد الغسيل'){
+            if($this->booking_sheet_words[0][$this->google_sheet->next_question] != 'موعد الغسيل'){
                 $this->next_question();
             }
         }
 
         $this->send_message($this->google_sheet->current_question);
-        if($this->booking_sheet_words[$this->google_sheet->next_question] == 'موعد الغسيل'){
+        if($this->booking_sheet_words[0][$this->google_sheet->next_question] == 'موعد الغسيل'){
             $this->appointments();
         }
     }
