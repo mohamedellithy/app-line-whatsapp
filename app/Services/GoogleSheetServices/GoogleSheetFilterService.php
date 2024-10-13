8cc292faf1a2be72ec04b239aedc0503564740c5
mohamedellithy <develomohamed22@gmail.com>
2024-10-13 12:33:53 +0300

    update 91

--- app/Services/GoogleSheetServices/GoogleSheetFilterService.php
+++ app/Services/GoogleSheetServices/GoogleSheetFilterService.php
@@ -64,8 +64,8 @@ class GoogleSheetFilterService extends GoogleSheetOperation {
             foreach($this->booking_appointments as $key => $booking_appointment):
                 $Max_Date = strtotime('+30 days');
                 $Min_Date = strtotime("+1 days");
-
-                if(($Max_Date >= strtotime($booking_appointment[0])) && ($Min_Date <= strtotime($booking_appointment[0]))){
+                $handle_date = date('Y-m-d',strtotime($booking_appointment[0]));
+                if(($Max_Date >= strtotime($handle_date)) && ($Min_Date <= strtotime($handle_date))){
                     $need_message .= '#'.$key.' => '.$booking_appointment[0]."\n";
                 }
 
