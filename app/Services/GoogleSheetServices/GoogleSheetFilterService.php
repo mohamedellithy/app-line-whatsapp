57ccaf27c5776f854d1b484ea8b7cc12233e2e8d
mohamedellithy <develomohamed22@gmail.com>
2024-10-13 12:42:22 +0300

    update 91

--- app/Services/GoogleSheetServices/GoogleSheetFilterService.php
+++ app/Services/GoogleSheetServices/GoogleSheetFilterService.php
@@ -3,7 +3,7 @@
 use App\Models\GoogleSheetAutoReplay;
 use App\Services\GoogleSheetServices\AccountService;
 use App\Services\GoogleSheetServices\GoogleSheetOperation;
-
+use Carbon\Carbon;
 class GoogleSheetFilterService extends GoogleSheetOperation {
     use AccountService;
     public $booking_sheet_words = [];
@@ -65,6 +65,7 @@ class GoogleSheetFilterService extends GoogleSheetOperation {
                 $Max_Date = strtotime('+30 days');
                 $Min_Date = strtotime("+1 days");
                 $handle_date = date('Y-m-d',strtotime($booking_appointment[0]));
+                \Log::info($handle_date);
                 if(($Max_Date >= strtotime($handle_date)) && ($Min_Date <= strtotime($handle_date))){
                     $need_message .= '#'.$key.' => '.$booking_appointment[0]."\n";
                 }
