ac3f7611147a9e153a1ded81eb9023071ae83098
mohamedellithy <develomohamed22@gmail.com>
2025-01-01 14:25:54 +0200

    add api v1 for account part-1

--- app/Services/SallaServices/CustomerCreated.php
+++ app/Services/SallaServices/CustomerCreated.php
@@ -66,7 +66,7 @@ class CustomerCreated implements AppEvent{
         ])->first();
         if( (!$account) || ($account->token == null)) return;
 
-        $lock = Cache::lock('event-'.$this->data['event'].'-'.$this->data['merchant'].'-'.$this->data['data']['id'], 10);
+        $lock = Cache::lock('event-'.$this->data['event'].'-'.$this->data['merchant'].'-'.$this->data['data']['id'],60);
         if($lock->get()){
             $attrs = formate_customer_details($this->data);
             $app_event = EventStatus::updateOrCreate([
