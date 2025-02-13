1a8604f29e454ed8f59a73ca5397f3b376634c84
mohamedellithy <develomohamed22@gmail.com>
2025-02-13 13:31:07 +0200

    fist

--- app/Services/SallaServices/ManualReviewRequest.php
+++ app/Services/SallaServices/ManualReviewRequest.php
@@ -65,7 +65,7 @@ class ManualReviewRequest implements AppEvent{
         ])->first();
         if( (!$account) || ($account->token == null)) return;
 
-        $lock = Cache::lock('event-'.$this->data['event'].'-'.$this->data['merchant'].'-'.$this->data['data']['id'], 30);
+        $lock = Cache::lock('event-review-'.$this->data['event'].'-'.$this->data['merchant'].'-'.$this->data['data']['id'], 30);
         if($lock->get()){
             $attrs = formate_order_details($this->data);
             DB::beginTransaction();
