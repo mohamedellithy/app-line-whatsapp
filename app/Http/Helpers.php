2df88424ab95ce16f4d3d4d5afc17dc5899643e4
mohamedellithy <develomohamed22@gmail.com>
2025-03-17 14:02:49 +0200

    add v2

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -282,8 +282,8 @@ function message_order_params($message_to_send = '',$attrs = []){
 
         elseif($variable == "تفاصيل_منتجات_الطلبية"){
             foreach ($attrs["items"] as $item){
-                $total_amount  = isset($item['amounts']) ? $item['amounts']['total']['amount'] : $item['total']['amount'];
-                $total_currency  = isset($item['amounts']) ? $item['amounts']['total']['currency'] : $item['total']['currency'];
+                $total_amount  = isset($item['amounts']) ? $item['amounts']['total']['amount'] : (isset($item['total']['amount']) ?: '');
+                $total_currency  = isset($item['amounts']) ? $item['amounts']['total']['currency'] : (isset($item['total']['currency']) ?: '');
                 $product_list[] = $item['name'].'  :  '.$item['quantity'].'  :  '.$total_amount.''.$total_currency;
             }
 
