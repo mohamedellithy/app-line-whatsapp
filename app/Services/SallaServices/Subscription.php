9d425039001c8992a5a64657426a5dd20eaee25c
mohamedellithy <develomohamed22@gmail.com>
2025-03-20 00:50:20 +0200

    add v9

--- app/Services/SallaServices/Subscription.php
+++ app/Services/SallaServices/Subscription.php
@@ -74,6 +74,7 @@ class Subscription implements AppEvent{
             $new_team->pid         = $plan_id;
             $new_team->permissions = $package->permissions;
             $new_team->save();
+            \Log::info($package->permissions);
 
             $upgrade_plan = SpUser::where('id',$new_team->owner)->first();
             $upgrade_plan->plan = $plan_id;
