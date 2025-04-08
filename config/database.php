2151244488022717de3e774e79a7be10c83efd25
mohamedellithy <develomohamed22@gmail.com>
2025-04-08 18:41:40 +0200

    fix timeout

--- config/database.php
+++ config/database.php
@@ -1,8 +1,21 @@
 <?php 
 return [
+    'connections' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+        ]
+    ],
     'redis' => [
         'client' => 'predis',
-
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
